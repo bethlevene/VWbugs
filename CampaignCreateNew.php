169af1cec6c952d61883a59cae719a2f852a7464
B <contact@byronlevene.net>
2016-01-07 16:33:37 +0800

    Update and rename CampaignCreateNew.page to CampaignCreateNew.php

---
+++ CampaignCreateNew.php
@@ -0,0 +1,9 @@
+<?php /* create new marker */
+
+<form>
+<input title="MarkerName"></input>
+<input title="Author"></input>
+<input title="Description"></input>
+<input title="Thumbnail"></input>
+<input title="Link"></input>
+</form>
